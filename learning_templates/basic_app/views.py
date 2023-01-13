5870ce5c586d20845a59d20acfd02079820c1636
shubham <shubhamtandan77@gmail.com>
2023-01-13 18:34:24 +0530

    Update views.py

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     context_dict = {'text':'hello world','number':100}
+    context_dict = {'text':'hello world','number':100}
     return render(request,'basic_app/index.html',context_dict)
 
 def other(request):
