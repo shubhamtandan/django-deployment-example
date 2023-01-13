3eb361d61b2e2c115fc1ad7732eb73d19c043e96
shubham <shubhamtandan77@gmail.com>
2023-01-13 18:34:37 +0530

    Update views.py

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context_dict = {'text':'hello world','number':100}
-    context_dict = {'text':'hello world','number':100}
-    return render(request,'basic_app/index.html',context_dict)
+    c = {'text':'hello world','number':100}
+    c = {'text':'hello world','number':100}
+    return render(request,'basic_app/index.html',c)
 
 def other(request):
     return render(request,'basic_app/other.html')
